055717dd4873d5db200e50a45d1583ffa420a1af
Ruchit-Thummar9 <ruchit.thummar@tectonas.in>
2026-03-05 12:15:51 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify, request
 
-server = Flask(__name__)
+app = Flask(__name__)
 
 @server.route("/")
 def hello():
     return "Hello World"
 
 if __name__ == "__main__":
-    server.run()
+    app.run()
